2df0a5a50b3effb39ecf40ef5c62f478aa254706
ajeygore <ajeygore@gmail.com>
2023-09-25 20:15:04 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,5 +4,5 @@ LANGUAGE=en_US.UTF-8
 LANG=en_US.UTF-8
 localedef -i en_US -f UTF-8 en_US.UTF-8 && /usr/sbin/update-locale LANG=$LANG
 wget https://raw.githubusercontent.com/ajeygore/ubuntu-workstation/main/ubuntu.yml
-sudo RPI_USER=$RPI_USER ansible-playbook ubuntu.yml 
+sudo RPI_USER=$RPI_USER ansible-playbook ubuntu.yml
 sh -c "`curl -fsSL https://raw.githubusercontent.com/ajeygore/dotfiles/master/install.sh `"
